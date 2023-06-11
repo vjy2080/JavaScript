0a5b2d851bbe6b6ee5ed26a77ae4269e1e3b51d0
vijay prajapati <vjy2080@gmail.com>
2023-06-11 19:41:57 +0530

    Update files

--- Task/Task2/Task2.js
+++ Task/Task2/Task2.js
@@ -1,7 +1,9 @@
-function check(para, spanid) {
-       if (para.value == "") {
-        document.getElementById(spanid).innerHTML="<b>This field is required</b>"
-    } else {
-           document.getElementById(spanid).innerHTML=""
+function gmete(para, spanid) {
+       console.log(para);
+       console.log(para.value);
+       if (para.value === "") {
+           document.getElementById(spanid).innerHTML = "<b>This field is required</b>";
+       } else {
+           document.getElementById(spanid).innerHTML = "";
        }
-}
+   }
