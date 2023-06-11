7c694a31c6deb9492db648057d611daaaf739eee
vijay prajapati <vjy2080@gmail.com>
2023-06-11 15:40:04 +0530

    Update files

--- Task/Task2/Task2.js
+++ Task/Task2/Task2.js
@@ -1,50 +1,9 @@
-function login() {
-
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var birthdate = document.getElementById("birthdate").value;
-    var phone = document.getElementById("phone").value;
-    var email = document.getElementById("email").value;
-    console.log("username => " + username)
-    console.log("password => " + password)
-    console.log("birthdate => " + birthdate)
-    console.log("phone => " + phone)
-    console.log("email => " + email)
-
-    if (username == "") {
-        document.getElementById("usernameerror").innerText = "Username required"
+function checkrequire(parameter,spanid){
+    if (parameter == "") {
+        console.log("Not okay");
+        // document.getElementById(spanid).innerText="This Field is required"
     } else {
-        document.getElementById("usernameerror").innerText = ""
+        console.log("okay");
+        // document.getElementById(spanid).innerText=""
     }
-
-    if (password == "") {
-        document.getElementById("passworderror").innerText = "Password required"
-    }
-
-    else {
-        document.getElementById("passworderror").innerText = ""
-    }
-
-    if (birthdate == "") {
-        document.getElementById("birthdateerror").innerText = "Birth date required"
-    } else {
-        document.getElementById("birthdateerror").innerText = ""
-    }
-
-    if (phone == "") {
-        document.getElementById("phoneerror").innerText = "Phone No. required"
-    } else {
-        document.getElementById("phoneerror").innerText = ""
-    }
-
-    if (email == "") {
-        document.getElementById("emailerror").innerText = "Email required"
-    }
-
-    else {
-        document.getElementById("emailerror").innerText = ""
-    }
-
 }
-
-function Blur() {}
