489a9d403bd1e153e8473d88f5dbfed2826aa02f
vijay prajapati <vjy2080@gmail.com>
2023-06-12 00:04:54 +0530

    Update files

--- Task/Task5/Task5.js
+++ Task/Task5/Task5.js
@@ -1,7 +1,27 @@
-let inputData = document.querySelectorAll('input');
+inputData = document.querySelectorAll('input');
 // console.log(inputData);
 inputData.forEach(element => {
     element.addEventListener("blur",function () {
-        console.log("called",this.value)
-    })
+        // console.log("called",this.value)
+        const newDiv = document.createElement("span");
+        const pText = document.createTextNode("This field is required");
+        newDiv.appendChild(pText);
+
+        // console.log(newDiv);
+        // console.log(this.value);
+        console.log("this.nextSibling",this.nextSibling.nodeName);
+
+
+    if (this.value == "") {
+      if (this.nextSibling && this.nextSibling.nodeName == "SPAN") {
+        this.nextSibling.remove();
+      } else {
+        this.after(newDiv);
+      }
+    } else {
+      if (this.nextSibling && this.nextSibling.nodeName == "SPAN") {
+        this.nextSibling.remove();
+      }
+    }
+  });
 });
