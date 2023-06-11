13e2b7135220ed3dfe2c7b474f2913f892211e42
vijay prajapati <vjy2080@gmail.com>
2023-06-11 17:13:36 +0530

    Update files

--- Task/Task2/Task2.js
+++ Task/Task2/Task2.js
@@ -2,6 +2,6 @@ function check(para, spanid) {
        if (para.value == "") {
         document.getElementById(spanid).innerHTML="<b>This field is required</b>"
     } else {
-           document.getElementById(spanid).innerHTML="<b></b>"
+           document.getElementById(spanid).innerHTML=""
        }
 }
